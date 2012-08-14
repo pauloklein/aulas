9d7ff8de74bd2cfa0255f51c3da8f65b5658b66b
651020021 <651020021@Sala603-15.educcbx.senacrs.com.br>
2012-08-13 21:26:28 -0300

    teste2

---
+++ entregas/aula3/src/main/java/br/com/senac/alp/aula3/App.java
@@ -0,0 +1,13 @@
+package br.com.senac.alp.aula3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
