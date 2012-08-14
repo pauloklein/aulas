fe147011029468916f8049a6559b48c2d3884903
651020021 <651020021@Sala603-15.educcbx.senacrs.com.br>
2012-08-13 21:47:42 -0300

    teste3

---
+++ entregas/aula3/src/main/java/br/com/senac/alp/aula3/Pessoa.java
@@ -0,0 +1,17 @@
+package br.com.senac.alp.aula3;
+
+public class Pessoa {
+	
+	private String nome = null;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+}
